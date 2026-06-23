61751ec8ad422930ea40e5e950e9ae6b7ffe5a2c
Sameer Kushwaha <sameerkushwaha1610@gmail.com>
2026-06-23 06:27:42 +0530

    updated operation

--- 04_operation.js
+++ 04_operation.js
@@ -4,39 +4,39 @@
 
 let value=23
 let negvalue=-value
-// console.log(negvalue)
-/*
+console.log(negvalue)
+
 console.log(2+2)
 console.log(3-2)
 console.log(3*2)
 console.log(3**5)
 console.log(5/2)
-console.log(8%5)*/
+console.log(8%5)
 
-// let str1="hii"
-// let str2="  sameer"
+let str1="hii"
+let str2="  sameer"
 
-// let str3=str1+str2
-// console.log(str3)
+let str3=str1+str2
+console.log(str3)
 
-// console.log("1" + 2)
-// console.log(2+"6")
-// console.log(2+3+3+"5")
-// console.log("5"+3+4+2)
-// console.log(2+4+"4"+4+5)
+console.log("1" + 2)
+console.log(2+"6")
+console.log(2+3+3+"5")
+console.log("5"+3+4+2)
+console.log(2+4+"4"+4+5)
 
 /*In JavaScript, the + operator has two meanings:
 Addition (when both operands are numbers)
 String concatenation (when at least one operand is a string)
 JavaScript evaluates expressions from left to right.*/
 
-// console.log(true)
-// console.log(+true)
-// console.log(+"")
+console.log(true)
+console.log(+true)
+console.log(+"")
 
-// let num1,num2,num3
-// num1=num2=num3=2+3
-// console.log(num1,num2,num3)
+let num1,num2,num3
+num1=num2=num3=2+3
+console.log(num1,num2,num3)
 
 
 
