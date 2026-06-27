8304b85d3105f26008c56ab6aadb9f764ba7bb1d
Sameer Kushwaha <sameerkushwaha1610@gmail.com>
2026-06-27 20:24:38 +0530

    Comparison In Js

--- 05_comparison.js
+++ 05_comparison.js
@@ -1,50 +1,50 @@
-// // ---------------------------------
-// // JavaScript Comparison Operators:
-// // ---------------------------------
+// ---------------------------------
+// JavaScript Comparison Operators:
+// ---------------------------------
 
-// // ==  → Equal (type convert karta hai) value compare krta hai time aane prr conert bhi krta hai type
-// console.log(3=="3");
-// console.log(3==3);
-// console.log(3==4);
-// console.log(true==1);
-// console.log(false ==0);
+// ==  → Equal (type convert karta hai) value compare krta hai time aane prr conert bhi krta hai type
+console.log(3=="3");
+console.log(3==3);
+console.log(3==4);
+console.log(true==1);
+console.log(false ==0);
 
-// // === → Strict Equal (value aur type dono same hone chahiye)
-// console.log(3==="3");
-// console.log(3===3);
-// console.log(3===4);
-// console.log(true===1);
-// console.log(false ===0);
+// === → Strict Equal (value aur type dono same hone chahiye)
+console.log(3==="3");
+console.log(3===3);
+console.log(3===4);
+console.log(true===1);
+console.log(false ===0);
 
-// // !=  → Not Equal
-// console.log(3!="3");
-// console.log(3!=3);
-// console.log(3!=4);
-// console.log(true!=1);
-// console.log(false !=0);
+// !=  → Not Equal
+console.log(3!="3");
+console.log(3!=3);
+console.log(3!=4);
+console.log(true!=1);
+console.log(false !=0);
 
-// // !== → Strict Not Equal
-// console.log(3!=="3")
-// console.log(3!==3)
-// console.log(3!==4)
-// console.log(true!==1)
-// console.log(false !==0)
+// !== → Strict Not Equal
+console.log(3!=="3")
+console.log(3!==3)
+console.log(3!==4)
+console.log(true!==1)
+console.log(false !==0)
 
-// // > → Greater Than
-// console.log(3 > "2");
-// console.log(3 > 3);
-// console.log(3 > 4);
-// console.log(true > 1);
-// console.log(false > 0);
+// > → Greater Than
+console.log(3 > "2");
+console.log(3 > 3);
+console.log(3 > 4);
+console.log(true > 1);
+console.log(false > 0);
 
 // <   → Less Than
-// 5 < 10 // true
+5 < 10 // true
 
-// // > =  → Greater Than or Equal
-// > 10 >= 10 // true
+// > =  → Greater Than or Equal
+> 10 >= 10 // true
 
-// // <=  → Less Than or Equal
-// 5 <= 10 // true
+// <=  → Less Than or Equal
+5 <= 10 // true
 
 // Note:
 // JavaScript me === aur !== use karna better practice mana jata hai.
