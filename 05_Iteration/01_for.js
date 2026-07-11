abd955beb707b6e9179912c9ebf20c0592ae60f8
Sameer Kushwaha <sameerkushwaha1610@gmail.com>
2026-07-11 09:51:21 +0530

    Iteration

---
+++ 05_Iteration/01_for.js
@@ -0,0 +1,22 @@
+// for
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   let element = i;
+//   if (element == 5) {
+//     console.log(`5 is the best number`);
+//   }
+//   console.log(element);
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(`---------------Outer loop valuev: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value : ${j}`)
+//         console.log(`${i} * ${j} = ${i*j}`)
+        
+//     }
+// }
